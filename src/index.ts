712e8def893aa5e5daf5b1fb1cf25a2c783a86b3
InvisiBug <matthewjkavanagh@gmail.com>
2021-10-25 22:51:08 +0100

    Mongoose working

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-const express = require("express");
+import express from "express";
+// import mongoose from "mongoose";
 const app = express();
-const mongoose = require("mongoose");
 
-//connecting to mongodb
-mongoose.connect("mongodb://mongo/myappdb", (err: Error) => {
-  if (err) throw err;
-  console.log("connected to mongo");
-});
+//Import the mongoose module
+import mongoose from "mongoose";
+
+//Set up default mongoose connection
+var dbUrl = "mongodb://localhost";
+mongoose.connect(dbUrl);
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("Node app is running at localhost:" + app.get("port"));
 });
 
-console.log("hels");
+console.log("🚀 App Running");
+
+// setInterval(() => {}, 10);
