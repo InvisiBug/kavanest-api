b850737c609353bb008bd26a8fef99bc93eaddf5
InvisiBug <matthewjkavanagh@gmail.com>
2021-10-26 21:56:28 +0100

    Example working

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,59 @@
-import express from "express";
-import { connectToDB } from "./service/dbService";
-require("dotenv").config();
+import { readFileSync } from "fs";
+import path from "path";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-const app = express();
+import resolvers from "./resolvers";
 
-//* Environment dependant stuff
-const URI: string = process.env.URI ?? "";
-const DATABASE: string = process.env.DATABASE ?? "";
-const COLLECTION = process.env.COLLECTION ?? "";
+const { ApolloServer, gql } = require("apollo-server");
 
-const startServer = async () => {
-  const port: number = 8081;
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+// const typeDefs = gql`
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  app.listen({ port: port }, () => {
-    console.log("Node app is running at localhost:" + port);
-  });
-};
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     title: String
+//     author: String
+//   }
 
-connectToDB(URI, DATABASE, COLLECTION)
-  .then(() => {
-    startServer();
-  })
-  .catch((error: Error) => {
-    console.error(error, "Database connection failed");
-    process.exit();
-  });
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
 
-console.log("🚀 App Running");
+// const books = [
+//   {
+//     title: "The Awakening",
+//     author: "Kate Chopin",
+//   },
+//   {
+//     title: "City of Glass",
+//     author: "Paul Auster",
+//   },
+// ];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+// const resolvers = {
+//   Query: {
+//     books: () => books,
+//   },
+// };
+
+//* GraphQL server config
+const typeDefs = readFileSync(path.resolve(__dirname, "./schema/schema.graphql"), "utf-8");
+const schema = makeExecutableSchema({ resolvers, typeDefs });
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ schema, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ src/mocks/index.ts
@@ -0,0 +1,14 @@
+const mocks = [
+  {
+    title: "Matts Book",
+    author: "Kate Chopin",
+    other: "nope",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+    other: "nope",
+  },
+];
+
+export default mocks;
