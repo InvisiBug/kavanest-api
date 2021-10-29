269e3187a725bfb0d86a0667ea01d7688c3a2126
InvisiBug <matthewjkavanagh@gmail.com>
2021-10-29 22:50:40 +0100

    Added dotenv

--- src/database/stores/devices.ts
+++ src/database/stores/devices.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import { Floodlight, Radiator, Sensor } from "../schemas";
+require("dotenv").config();
 
-const devicesConnection = mongoose.createConnection("mongodb://localhost:27017/devices");
+const devicesConnection = mongoose.createConnection(process.env.URI ?? "");
 export const FloodLightStore = devicesConnection.model<FloodLight>("floodlight", Floodlight);
 export const RadiatorStore = devicesConnection.model<Radiator>("radiator", Radiator);
 export const SensorStore = devicesConnection.model<Sensor>("sensor", Sensor);
 
 devicesConnection.on("connected", () => {
-  console.log("🔗 Mongoose connected to " + "mongodb://localhost:27017/devices");
+  console.log("🔗 Mongoose connected to " + process.env.URI ?? "");
 });
 
 devicesConnection.on("error", (err) => {
