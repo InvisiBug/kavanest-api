23f7babce5299ea0f2f8b70788276e1b2ea9c74a
InvisiBug <matthewjkavanagh@gmail.com>
2021-12-24 01:06:43 +0000

    Added variables to .env file

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
 export { log } from "./components/log";
-export { mqttUrl } from "./components/mqttUrlGenerator";
-export { mongoUrl } from "./components/mongoUrlGenerator";
+// export { mqttUrl } from "./components/mqttUrlGenerator";
+// export { mongoUrl } from "./components/mongoUrlGenerator";
+
+export { mqttUrl, mongoUrl } from "./components/urlGenerator";
