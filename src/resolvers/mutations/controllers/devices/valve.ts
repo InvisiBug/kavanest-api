861d35c9dd0751fffcdcdce34f164c59312a00c0
InvisiBug <matthewjkavanagh@gmail.com>
2022-01-25 21:57:46 +0000

    Updated valve controll

--- src/resolvers/mutations/controllers/devices/valve.ts
+++ src/resolvers/mutations/controllers/devices/valve.ts
@@ -7,7 +7,7 @@ let client: mqtt.MqttClient = mqtt.connect(mqttUrl);
 export default async (_: any, { input }: Args) => {
   const { name, state } = input;
 
-  toggle(state, `${decamelize(name)} Radiator Valve Control`, "1", "0");
+  toggle(state, `${decamelize(name)} Valve Control`, "1", "0");
   const valve = await valveStore.findOneAndUpdate({ room: name }, { $set: { state } }, options);
   return valve.value;
 };
