05fc9f8cddc9d84264819d0f79845f7a0b67c7d9
InvisiBug <matthewjkavanagh@gmail.com>
2022-02-06 23:32:23 +0000

    Changes to heating room object

--- src/resolvers/mutations/controllers/room.ts
+++ src/resolvers/mutations/controllers/room.ts
@@ -3,15 +3,20 @@ import { options, roomStore } from "../../../database";
 export default async (_: any, { input }: Args) => {
   const { name, demand, overrideTime, disabled } = input;
 
-  console.log(input);
-
   let newOverrideTime: number;
   if (overrideTime) {
     newOverrideTime = offsetTimeMins(overrideTime);
   }
 
-  const data = await roomStore.findOneAndUpdate({ name }, { $set: { demand, overrideTime: newOverrideTime, disabled } }, options);
-  console.log(data.value);
+  const updatedRoom = {
+    ...(demand != undefined && { demand: input?.demand }),
+    ...(disabled != undefined && { disabled: input?.disabled }),
+    ...(overrideTime != undefined && { overrideTime: newOverrideTime }),
+  };
+
+  // console.log(name, updatedRoom);
+
+  const data = await roomStore.findOneAndUpdate({ name }, { $set: updatedRoom }, options);
   return data.value;
 };
 
